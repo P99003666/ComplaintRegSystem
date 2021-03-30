85f0ee4632446b034703053a4b4770437a311fa9
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-03-30 09:16:11 +0530

    Bean Added

--- project/src/main/java/com/ltts/project/ProjectApplication.java
+++ project/src/main/java/com/ltts/project/ProjectApplication.java
@@ -2,12 +2,18 @@ package com.ltts.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+		
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
 
 }
